68a2e308d3471b032d225d09535910054a16126d
Rafy43 <160065139+Rafy43@users.noreply.github.com>
2025-05-26 17:40:13 +0600

    Add files via upload

---
+++ Doubly_Linked_list.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+void forward_print(Node* &head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void backward_print(Node* tail){
+    Node* temp=tail;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->prev;
+    }
+}
+int main(){
+    Node* head=new Node(10);
+    Node* a=new Node(20);
+    Node* b=new Node(30);
+    Node* tail=new Node(40);
+
+    head->next=a;
+    a->prev=head;
+    a->next=b;
+    b->prev=a;
+    b->next=tail;
+    tail->prev=b;
+
+    forward_print(head);
+    backward_print(tail);
+}
